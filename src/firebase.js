6bd9d7854c76983f684b896cb3261ac91fbe47da
RizkyJH <firecalm2@gmail.com>
2026-04-16 17:27:59 +0700

    First build: SunVolt Dashboard with Firebase and Tailwind

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyApn4txSJN29ZRle9hOqMli0zfkya8Uh-Q",
+  authDomain: "sunvolt-admin.firebaseapp.com",
+  projectId: "sunvolt-admin",
+  storageBucket: "sunvolt-admin.firebasestorage.app",
+  messagingSenderId: "759944928251",
+  appId: "1:759944928251:web:602607c4d46d04bf86c95e",
+  measurementId: "G-Y6X03PP9EV"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
